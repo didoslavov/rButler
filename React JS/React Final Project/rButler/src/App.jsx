b613c763650e0703b1417a0051bd711be00bebb8
didkoslawow <didkoslawow@gmail.com>
2023-09-17 11:46:57 +0300

    initialize project

---
+++ React JS/React Final Project/rButler/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar.jsx';
+
+function App() {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+}
+
+export default App;
