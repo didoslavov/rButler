71db3ab475d3895dd261a729594d1fa1828fe2ec
didkoslawow <didkoslawow@gmail.com>
2023-09-21 13:34:34 +0300

    create user added

--- React JS/React Final Project/backend/src/controllers/usersController.js
+++ React JS/React Final Project/backend/src/controllers/usersController.js
@@ -4,10 +4,28 @@ const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
 
 const createUser = asyncHandler(async (req, res) => {
-    const { username, email, password, roles } = req.body;
+    const { username, email, password } = req.body;
 
-    if (!username || !email || !password || !Array.isArray(roles) || !roles.length) {
+    if (!username || !email || !password) {
         return res.status(400).json({ message: 'All fields are required!' });
+    }
+
+    const duplicateUser = await User.findOne({ username }).lean();
+    console.log(duplicateUser);
+    if (duplicateUser) {
+        return res.status(409).json({ message: 'Duplicate username' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userObject = { username, password: hashedPassword };
+
+    const user = User.create(userObject);
+
+    if (user) {
+        res.status(201).json({ message: `New user ${username} created!` });
+    } else {
+        res.status(400).json({ message: 'Invalid user data recieved!' });
     }
 });
 
