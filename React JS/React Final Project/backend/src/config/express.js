b5e254565064715f320db1b0cea7ad5996e55472
didkoslawow <didkoslawow@gmail.com>
2023-09-23 14:18:09 +0300

    added cookie-parser to the project

--- React JS/React Final Project/backend/src/config/express.js
+++ React JS/React Final Project/backend/src/config/express.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('../middlewares/cors.js');
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +9,7 @@ module.exports = (app) => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json({ extended: true }));
     app.use(cors());
+    app.use(cookieParser());
 
     app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
 };
