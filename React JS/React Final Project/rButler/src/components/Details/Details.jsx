3bed9367a3daa26996258d1df6db002561f8b73a
didkoslawow <didkoslawow@gmail.com>
2023-09-27 18:53:28 +0300

    added form popover on add household member clicked

--- React JS/React Final Project/rButler/src/components/Details/Details.jsx
+++ React JS/React Final Project/rButler/src/components/Details/Details.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { getUserHouseholdById } from '../../services/householdsService.js';
-import { SpeedDial, SpeedDialAction } from '@mui/material';
-import { AddHomeSharp, ExtensionSharp, HomeSharp, ModeEditSharp, ShareRounded } from '@mui/icons-material';
+import { IconButton, Paper, Popover, SpeedDial, SpeedDialAction, Typography } from '@mui/material';
+import { AddHomeSharp, Close, ExtensionSharp, HomeSharp, ModeEditSharp, ShareRounded } from '@mui/icons-material';
 
 const Details = () => {
     const token = localStorage.getItem('authToken');
     const { householdId } = useParams();
     const [household, setHousehold] = useState({});
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
 
     const actions = [
         {
             icon: (
-                <Link to="/" className="details-speed-dial-link">
+                <Link className="details-speed-dial-link" onClick={(event) => handlePopupOpen(event)}>
                     <AddHomeSharp />
                 </Link>
             ),
@@ -47,6 +49,16 @@ const Details = () => {
     useEffect(() => {
         getUserHouseholdById(householdId, token).then((h) => setHousehold(h));
     }, [token]);
+
+    const handlePopupOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+        setIsPopupOpen(true);
+    };
+
+    const handlePopupClose = () => {
+        setAnchorEl(null);
+        setIsPopupOpen(false);
+    };
 
     return (
         <div className="details-container">
@@ -87,6 +99,76 @@ const Details = () => {
                         ))}
                     </SpeedDial>
                 </div>
+                <Popover
+                    open={isPopupOpen}
+                    anchorEl={anchorEl}
+                    onClose={handlePopupClose}
+                    anchorOrigin={{
+                        vertical: 'center',
+                        horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}>
+                    <Paper
+                        sx={{
+                            padding: '20px',
+                            border: '1px solid var(--dark-blue)',
+                            backgroundColor: 'var(--light-grey)',
+                            color: 'var(--dark-blue)',
+                            '& form': {
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            },
+                            '& h4': {
+                                textAlign: 'center',
+                            },
+                            '& label': {
+                                marginTop: '5px',
+                                marginBottom: '5px',
+                            },
+                            '& .details-popup-input': {
+                                border: '1px solid var(--dark-blue)',
+                                padding: '5px',
+                                color: 'var(--dark-blue)',
+                                '&::placeholder': {
+                                    textAlign: 'center',
+                                },
+                            },
+                            '& .add-button': {
+                                marginTop: '5px',
+                                fontSize: '12px',
+                                padding: '6px',
+                            },
+                        }}>
+                        <IconButton
+                            aria-label="close"
+                            onClick={handlePopupClose}
+                            sx={{
+                                position: 'absolute',
+                                top: '2px',
+                                right: '0',
+                                '& .css-i4bv87-MuiSvgIcon-root': {
+                                    fontSize: '1rem',
+                                    color: 'var(--dark-blue)',
+                                },
+                            }}>
+                            <Close />
+                        </IconButton>
+                        <div className="popup-form-container"></div>
+                        <h4 className="border-bottom">Add household member</h4>
+                        <form className="popup-form">
+                            <label htmlFor="username">Username</label>
+                            <input className="details-popup-input" type="text" name="username" placeholder="Username to add?" />
+                            <label htmlFor="role">Role</label>
+                            <input className="details-popup-input" type="text" name="role" placeholder="Add user role" />
+                            <input type="submit" name="button" value="ADD" className="button-action add-button" />
+                        </form>
+                    </Paper>
+                </Popover>
             </div>
         </div>
     );
