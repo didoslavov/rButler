a7b67dd85e393f49f0d56f165a39337a5fd1f5bb
didkoslawow <didkoslawow@gmail.com>
2023-09-29 10:55:55 +0300

    prepopulate inputs when edit form i shown

--- React JS/React Final Project/rButler/src/components/EditHousehold/EditHousehold.jsx
+++ React JS/React Final Project/rButler/src/components/EditHousehold/EditHousehold.jsx
@@ -1,14 +1,6 @@
-import { MenuItem, Select } from '@mui/material';
-import React, { useState } from 'react';
-import { menuItemStyles, selectStyles } from '../../../public/styles/muiStyles/details.js';
+import React from 'react';
 
-const EditHousehold = () => {
-    const [role, setRole] = useState('Resident');
-
-    const onSelect = (e) => {
-        setRole(e.target.value);
-    };
-
+const EditHousehold = ({ household }) => {
     return (
         <>
             <div className="edit-household-container">
@@ -16,27 +8,11 @@ const EditHousehold = () => {
                     <label>
                         <span>Household name</span>
                     </label>
-                    <input type="text" className="input" />
+                    <input type="text" className="input" defaultValue={household.name} />
                     <label>
                         <span>Presentation</span>
                     </label>
-                    <input type="text" className="input" />
-                    <label>Roles</label>
-                    <Select
-                        MenuProps={{ MenuListProps: { disablePadding: true } }}
-                        sx={selectStyles}
-                        onChange={onSelect}
-                        labelId="role-select"
-                        id="role-select"
-                        value={role}
-                        label="Role">
-                        <MenuItem sx={menuItemStyles} value={'Master'}>
-                            Master
-                        </MenuItem>
-                        <MenuItem sx={menuItemStyles} value={'Resident'}>
-                            Resident
-                        </MenuItem>
-                    </Select>
+                    <input type="text" className="input" defaultValue={household.presentation} />
                     <div className="buttons-form">
                         <input type="submit" className="button-action edit-button" value={'Edit Household'} />
                         <button className="button-action delete-button">Delete</button>
