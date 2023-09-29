ae27090e5e541f96253094a2f5154ffaf86df04e
didkoslawow <didkoslawow@gmail.com>
2023-09-29 16:46:31 +0300

    added PATCH method to cors options

--- React JS/React Final Project/backend/src/middlewares/cors.js
+++ React JS/React Final Project/backend/src/middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = () => (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Auth');
     res.setHeader('access-control-expose-headers', 'Set-Cookie');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
