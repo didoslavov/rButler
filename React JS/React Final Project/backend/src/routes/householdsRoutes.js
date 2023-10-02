6911c5a35db4e540ce7de4fdcd700ca470a6cce1
didkoslawow <didkoslawow@gmail.com>
2023-10-02 09:21:45 +0300

    changed route for add member functionality

--- React JS/React Final Project/backend/src/routes/householdsRoutes.js
+++ React JS/React Final Project/backend/src/routes/householdsRoutes.js
@@ -3,11 +3,11 @@ const householdController = require('../controllers/householdsController.js');
 const authMiddleware = require('../middlewares/authMiddleware.js');
 
 router.route('/').get(householdController.getAllHouseholds);
-router.route('/add-member').post(authMiddleware(), householdController.addHouseholdMember);
 router.route('/create').post(authMiddleware(), householdController.createHouseholds);
 router.route('/user-households/:userId').get(authMiddleware(), householdController.getUserHouseholds);
 router.route('/update/:householdId').patch(authMiddleware(), householdController.updateHouseholds);
 router.route('/delete/:householdId').delete(authMiddleware(), householdController.deleteHouseholds);
+router.route('/:householdId/add-member').post(authMiddleware(), householdController.addHouseholdMember);
 router.route('/:householdId').get(authMiddleware(), householdController.getHouseholdById);
 
 module.exports = router;
