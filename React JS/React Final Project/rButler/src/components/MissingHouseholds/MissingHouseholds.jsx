631af9fc0ab34ce0dcdad84e482ab0e3e74109aa
didkoslawow <didkoslawow@gmail.com>
2023-10-05 14:14:24 +0300

    reuse missinghouseholds component

---
+++ React JS/React Final Project/rButler/src/components/MissingHouseholds/MissingHouseholds.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MissingHouseholds = ({ token }) => {
+    return (
+        <div className="missing-household">
+            <p>
+                Regrettably, it appears that no households have been created under your esteemed account. Shall I assist you in
+                the initiation of this noble endeavor, kindly select below?
+            </p>
+            {token ? (
+                <Link to="/households/create" className="button-action">
+                    Create household
+                </Link>
+            ) : (
+                <Link to="/profile/auth" className="button-action">
+                    Sign in to create household
+                </Link>
+            )}
+        </div>
+    );
+};
+
+export default MissingHouseholds;
