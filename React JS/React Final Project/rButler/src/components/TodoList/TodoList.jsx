8dbbfb3d368c8ac2afaad74f9e042d5b42ddf6d8
didkoslawow <didkoslawow@gmail.com>
2023-10-07 17:56:48 +0300

    add item implemented in Todo list component

--- React JS/React Final Project/rButler/src/components/TodoList/TodoList.jsx
+++ React JS/React Final Project/rButler/src/components/TodoList/TodoList.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Divider, IconButton, Tooltip } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AlertDialog from '../ConfirmModal/AlertDialog.jsx';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { addListItem, getListById } from '../../services/listsService.js';
 
 const TodoList = () => {
+    const { listId } = useParams();
+    const navigate = useNavigate();
     const [open, setOpenDialog] = useState(false);
+    const [items, setItems] = useState([]);
+    const { register, handleSubmit, reset } = useForm();
 
     const handleClickDelete = () => {
         setOpenDialog(true);
@@ -17,6 +23,20 @@ const TodoList = () => {
         setOpenDialog(false);
     };
 
+    const onAddItem = async ({ text }) => {
+        const list = await addListItem(listId, { text });
+        reset();
+        setItems(list.items);
+    };
+
+    const handleGoBack = () => {
+        navigate(-1);
+    };
+
+    useEffect(() => {
+        getListById(listId).then((list) => setItems(list?.items));
+    }, []);
+
     return (
         <>
             {open && <AlertDialog handleClose={handleClose} open={open} message={'You are about to delete the list.'} />}
@@ -25,136 +45,32 @@ const TodoList = () => {
                 <div className="list-container">
                     <h2 className="welcome-list">TODO List</h2>
                     <div className="form-container">
-                        <form className="todo-form-list">
+                        <form className="todo-form-list" onSubmit={handleSubmit(onAddItem)}>
                             <label className="list-form-label">
                                 <span>Todo</span>
                             </label>
-                            <input type="text" className="input item-input" />
+                            <input type="text" className="input todo-item-input" {...register('text')} />
                             <input type="submit" className="submit button list-submit" value={'Add todo'} />
                         </form>
                         <h4>Items</h4>
                         <ul className="lists">
-                            <li className="list">
-                                <span className="list-span-name">Bread</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Onion</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Milk</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Salt</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Apples</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Oranges</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Lemons</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Sugar</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Coffee</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Beer</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
-                            <li className="list">
-                                <span className="list-span-name">Peanuts</span>
-                                <Divider
-                                    orientation="vertical"
-                                    flexItem
-                                    sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
-                                />
-                                <IconButton aria-label="delete" size="medium">
-                                    <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
-                                </IconButton>
-                            </li>
+                            {items.length ? (
+                                items.map((item) => (
+                                    <li className="list" key={item._id}>
+                                        <span className="list-span-name">{item.text}</span>
+                                        <Divider
+                                            orientation="vertical"
+                                            flexItem
+                                            sx={{ backgroundColor: 'var(--dark-pink)', margin: '6px 0 0 8px' }}
+                                        />
+                                        <IconButton aria-label="delete" size="medium">
+                                            <CheckIcon sx={{ color: 'var(--dark-blue)' }} fontSize="inherit" />
+                                        </IconButton>
+                                    </li>
+                                ))
+                            ) : (
+                                <p>No items</p>
+                            )}
                         </ul>
                     </div>
                 </div>
@@ -163,11 +79,9 @@ const TodoList = () => {
                         title="Back to Lists"
                         placement="top"
                         sx={{ color: 'var(--dark-blue)', backgroundColor: 'var(--light-grey)' }}>
-                        <Link to={'/households/details/'}>
-                            <IconButton aria-label="back" size="large" sx={{ color: 'var(--dark-blue)' }}>
-                                <ArrowBackIcon fontSize="inherit" />
-                            </IconButton>
-                        </Link>
+                        <IconButton aria-label="back" size="large" sx={{ color: 'var(--dark-blue)' }} onClick={handleGoBack}>
+                            <ArrowBackIcon fontSize="inherit" />
+                        </IconButton>
                     </Tooltip>
                     <Tooltip
                         title="Delete List"
