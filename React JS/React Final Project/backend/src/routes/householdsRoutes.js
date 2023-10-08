e08318359d055ad99ca2eb873eceab5ea3b2dd8d
didkoslawow <didkoslawow@gmail.com>
2023-10-08 10:57:20 +0300

    fixed households routes to use auth middleware correctly

--- React JS/React Final Project/backend/src/routes/householdsRoutes.js
+++ React JS/React Final Project/backend/src/routes/householdsRoutes.js
@@ -3,12 +3,12 @@ const householdController = require('../controllers/householdsController.js');
 const authMiddleware = require('../middlewares/authMiddleware.js');
 
 householdsRouter.route('/').get(householdController.getAllHouseholds);
-householdsRouter.route('/create').post(authMiddleware(), householdController.createHouseholds);
-householdsRouter.route('/user-households/:userId').get(authMiddleware(), householdController.getUserHouseholds);
-householdsRouter.route('/update/:householdId').patch(authMiddleware(), householdController.updateHouseholds);
-householdsRouter.route('/delete/:householdId').delete(authMiddleware(), householdController.deleteHouseholds);
-householdsRouter.route('/:householdId/add-member').post(authMiddleware(), householdController.addHouseholdMember);
-householdsRouter.route('/:householdId/remove-member').post(authMiddleware(), householdController.removeHouseholdMember);
+householdsRouter.route('/create').post(authMiddleware, householdController.createHouseholds);
+householdsRouter.route('/user-households/:userId').get(authMiddleware, householdController.getUserHouseholds);
+householdsRouter.route('/update/:householdId').patch(authMiddleware, householdController.updateHouseholds);
+householdsRouter.route('/delete/:householdId').delete(authMiddleware, householdController.deleteHouseholds);
+householdsRouter.route('/:householdId/add-member').post(authMiddleware, householdController.addHouseholdMember);
+householdsRouter.route('/:householdId/remove-member').post(authMiddleware, householdController.removeHouseholdMember);
 householdsRouter.route('/:householdId').get(householdController.getHouseholdById);
 
 module.exports = householdsRouter;
