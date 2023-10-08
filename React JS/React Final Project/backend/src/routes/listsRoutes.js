ca235eec08bd84e55d28877ab9953f32ee294b07
didkoslawow <didkoslawow@gmail.com>
2023-10-08 12:54:37 +0300

    removed auth middleware from specific list route

--- React JS/React Final Project/backend/src/routes/listsRoutes.js
+++ React JS/React Final Project/backend/src/routes/listsRoutes.js
@@ -7,6 +7,6 @@ listsRouter.route('/create').post(authMiddleware, listsController.createList);
 listsRouter.route('/:listId/delete').delete(authMiddleware, listsController.deleteList);
 listsRouter.route('/:listId/add-item').post(authMiddleware, listsController.addItem);
 listsRouter.route('/:listId/delete-item/:itemId').delete(authMiddleware, listsController.deleteItem);
-listsRouter.route('/:listId').get(authMiddleware, listsController.getListById);
+listsRouter.route('/:listId').get(listsController.getListById);
 
 module.exports = listsRouter;
