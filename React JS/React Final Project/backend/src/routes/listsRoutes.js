886649fbc9ad1448ed2534cb04bb66d270483e09
didkoslawow <didkoslawow@gmail.com>
2023-10-08 10:57:40 +0300

    added auth middleware for protected routes in lists

--- React JS/React Final Project/backend/src/routes/listsRoutes.js
+++ React JS/React Final Project/backend/src/routes/listsRoutes.js
@@ -1,9 +1,12 @@
 const listsRouter = require('express').Router();
 const listsController = require('../controllers/listsController');
+const authMiddleware = require('../middlewares/authMiddleware.js');
 
 listsRouter.route('/').get(listsController.getAllLists);
-listsRouter.route('/create').post(listsController.createList);
-listsRouter.route('/:listId/add-item').post(listsController.addItem);
-listsRouter.route('/:listId').get(listsController.getListById);
+listsRouter.route('/create').post(authMiddleware, listsController.createList);
+listsRouter.route('/:listId/delete').delete(authMiddleware, listsController.deleteList);
+listsRouter.route('/:listId/add-item').post(authMiddleware, listsController.addItem);
+listsRouter.route('/:listId/delete-item/:itemId').delete(authMiddleware, listsController.deleteItem);
+listsRouter.route('/:listId').get(authMiddleware, listsController.getListById);
 
 module.exports = listsRouter;
