2c548551a5db621e6c80a3f735366a54cba4b6ca
didkoslawow <didkoslawow@gmail.com>
2023-10-09 19:12:44 +0300

    removed token, use user instead

--- React JS/React Final Project/rButler/src/App.jsx
+++ React JS/React Final Project/rButler/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useParams } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
@@ -12,14 +12,15 @@ import Auth from './components/Auth/Auth.jsx';
 import ShoppingList from './components/ShoppingList/ShoppingList.jsx';
 import TodoList from './components/TodoList/TodoList.jsx';
 import AllHouseholds from './components/AllHouseholds/AllHouseholds.jsx';
+import { getUserData } from './utils/userData.js';
 
 function App() {
-    const [token, setToken] = useState(localStorage.getItem('userData')?.token);
-    const [user, setUser] = useState(localStorage.getItem('userData'));
+    const [user, setUser] = useState(getUserData());
+    const [token, setToken] = useState(user?.token);
 
     useEffect(() => {
-        const localStorageToken = localStorage.getItem('userData')?.token;
-        const localStorageUser = localStorage.getItem('userData');
+        const localStorageUser = getUserData();
+        const localStorageToken = localStorageUser?.token;
 
         setToken(localStorageToken);
         setUser(localStorageUser);
@@ -33,16 +34,16 @@ function App() {
 
     return (
         <>
-            <Navbar onLogout={onLogout} token={token} user={user} />
+            <Navbar onLogout={onLogout} user={user} />
             <Routes>
-                <Route path="/" element={<Home token={token} setToken={setToken} user={user} />} />
+                <Route path="/" element={<Home user={user} />} />
                 <Route path="/profile/auth" element={<Auth setUser={setUser} setToken={setToken} />} />
-                <Route path="/households" element={<AllHouseholds token={token} />} />
-                <Route path="/households/details/:householdId" element={<Details />} />
-                <Route path="/households/create" element={<CreateHouseholdForm />} />
-                <Route path="/households/:userId" element={<MyHouseholds token={token} user={user} />} />
-                <Route path="/lists/shopping/:listId" element={<ShoppingList token={token} />} />
-                <Route path="/lists/todo/:listId" element={<TodoList token={token} />} />
+                <Route path="/households" element={<AllHouseholds user={user} />} />
+                <Route path="/households/details/:householdId" element={<Details user={user} />} />
+                <Route path="/households/create" element={<CreateHouseholdForm user={user} />} />
+                <Route path="/households/:userId" element={<MyHouseholds user={user} />} />
+                <Route path="/lists/shopping/:listId" element={<ShoppingList user={user} />} />
+                <Route path="/lists/todo/:listId" element={<TodoList user={user} />} />
                 <Route path="*" element={<Default />}></Route>
             </Routes>
         </>
