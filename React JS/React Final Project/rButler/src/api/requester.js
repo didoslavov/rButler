11f0cf46f65dfca575cac58ee08b2c95b10f7516
didkoslawow <didkoslawow@gmail.com>
2023-10-09 17:05:15 +0300

    added abstraction for fetching data

---
+++ React JS/React Final Project/rButler/src/api/requester.js
@@ -0,0 +1,48 @@
+import { clearUserData, getUserData } from '../utils/userData.js';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {},
+    };
+
+    if (data != undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const user = getUserData();
+
+    if (user) {
+        options.headers['Auth'] = user.token;
+    }
+
+    try {
+        const response = await fetch(BASE_URL + url, options);
+
+        if (response.status === 403) {
+            clearUserData();
+        }
+
+        if (response.status === 204) {
+            return { error: 'No content' };
+        }
+
+        if (!response.ok) {
+            const error = await response.json();
+            return error;
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const del = request.bind(null, 'DELETE');
