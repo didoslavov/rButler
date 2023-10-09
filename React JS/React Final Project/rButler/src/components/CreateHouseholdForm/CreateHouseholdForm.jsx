e6a5f45368f76a420fda7f734eaafe162af25d1a
didkoslawow <didkoslawow@gmail.com>
2023-10-09 19:14:01 +0300

    added notification and refactor fetching data

--- React JS/React Final Project/rButler/src/components/CreateHouseholdForm/CreateHouseholdForm.jsx
+++ React JS/React Final Project/rButler/src/components/CreateHouseholdForm/CreateHouseholdForm.jsx
@@ -1,50 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createHousehold } from '../../services/householdsService.js';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import Notification from '../Notification/Notification.jsx';
 
-const CreateHouseholdForm = () => {
+const CreateHouseholdForm = ({ user }) => {
     const { register, handleSubmit } = useForm();
     const navigate = useNavigate();
-    const token = localStorage.getItem('userData').token;
-    const master = JSON.parse(localStorage.getItem('userData')).id;
+    const [notification, setNotification] = useState('');
+    const [severity, setSeverity] = useState('');
+    const [notify, setNotify] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const onCreateHousehold = async ({ name, presentation }) => {
         try {
             if (!name || !presentation) {
-                throw new Error('All fields are required!');
+                throw ['All fields are required!'];
             }
 
-            const res = await createHousehold({ name, presentation, token, master });
+            await createHousehold({ name, presentation, master: user.id });
 
-            if (res === 'Unauthorized') {
-                navigate('/profile/auth');
-            }
-
-            navigate('/households/' + master);
+            navigate('/households/' + user.id);
         } catch (error) {
-            console.error(error);
+            setSeverity('error');
+            setNotification(error);
+            setOpen(true);
+            setNotify(true);
         }
     };
 
     return (
-        <div className="household-container">
-            <div className="create-household-container">
-                <h2 className="welcome welcome-household">Welcome, esteemed guest, to the creation of your digital household.</h2>
-                <form className="form-household" onSubmit={handleSubmit(onCreateHousehold)}>
-                    <label>
-                        <span>Household name</span>
-                        <input type="text" className="input" {...register('name')} />
-                    </label>
-                    <label>
-                        <span>Presentation</span>
-                        <input type="text" className="input" {...register('presentation')} />
-                    </label>
-                    <input type="submit" className="submit button" value={'Create Household'} />
-                </form>
+        <>
+            <div className="household-container">
+                <div className="create-household-container">
+                    <h2 className="welcome welcome-household">
+                        Welcome, esteemed guest, to the creation of your digital household.
+                    </h2>
+                    <form className="form-household" onSubmit={handleSubmit(onCreateHousehold)}>
+                        <label>
+                            <span>Household name</span>
+                            <input type="text" className="input" {...register('name')} />
+                        </label>
+                        <label>
+                            <span>Presentation</span>
+                            <input type="text" className="input" {...register('presentation')} />
+                        </label>
+                        <input type="submit" className="submit button" value={'Create Household'} />
+                    </form>
+                </div>
+                <img src="/create-household.jpg" alt="landing image" className="household-image" />
             </div>
-            <img src="/create-household.jpg" alt="landing image" className="household-image" />
-        </div>
+            {notify && <Notification open={open} setOpen={setOpen} message={notification} severity={severity} />}
+        </>
     );
 };
 
