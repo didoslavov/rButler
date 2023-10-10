9ad5a156661cdb6b1be36406401c251165e5b358
didkoslawow <didkoslawow@gmail.com>
2023-10-10 14:25:58 +0300

    add endpoints in lists service

--- React JS/React Final Project/rButler/src/services/listsService.js
+++ React JS/React Final Project/rButler/src/services/listsService.js
@@ -1,21 +1,22 @@
 import { del, get, post } from '../api/requester.js';
+import { listsEndpoints } from '../api/endpoints.js';
 
 export const getListById = async (id) => {
-    return await get('/lists/' + id);
+    return await get(listsEndpoints.getListById + id);
 };
 
 export const createList = async (list) => {
-    return await post('/lists/create', list);
+    return await post(listsEndpoints.createList, list);
 };
 
-export const removeList = async (id) => {
-    return await del('/lists/' + id + '/delete');
+export const removeList = async (listId) => {
+    return await del(listsEndpoints.removeList(listId));
 };
 
-export const addListItem = async (id, listItem) => {
-    return await post('/lists/' + id + '/add-item', listItem);
+export const addListItem = async (listId, listItem) => {
+    return await post(listsEndpoints.addListItem(listId), listItem);
 };
 
-export const removeListItem = async (itemId, listId, token) => {
-    return await del('/lists/' + listId + '/delete-item/' + itemId);
+export const removeListItem = async (itemId, listId) => {
+    return await del(listsEndpoints.removeListItem(listId) + itemId);
 };
