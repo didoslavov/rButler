232041449a337ff99978e239f92b11d291fc713d
didkoslawow <didkoslawow@gmail.com>
2023-10-10 14:25:45 +0300

    added endpoints in household service

--- React JS/React Final Project/rButler/src/services/householdsService.js
+++ React JS/React Final Project/rButler/src/services/householdsService.js
@@ -1,35 +1,35 @@
 import { del, get, patch, post } from '../api/requester.js';
+import { householdEndpoints } from '../api/endpoints.js';
 
 export const getAllHouseholds = async (query = '') => {
-    return await get(`/households?search=${query}`);
+    return await get(householdEndpoints.getAllHouseholds(query));
 };
 
 export const getUserHouseholds = async (userId) => {
-    return await get('/households/' + userId + '/user-households');
+    return await get(householdEndpoints.getUserHouseholds(userId));
 };
 
 export const createHousehold = async (household) => {
-    return await post('/households/create', household);
+    return await post(householdEndpoints.createHousehold, household);
 };
 
-export const getUserHouseholdById = async (id) => {
-    return await get('/households/' + id);
+export const getUserHouseholdById = async (userId) => {
+    return await get(householdEndpoints.getUserHouseholdById + userId);
 };
 
 export const addUserToHousehold = async (user, householdId) => {
-    return await post('/households/' + householdId + '/add-member', user);
+    return await post(householdEndpoints.addUserToHousehold(householdId), user);
 };
 
 export const removeUserFromHousehold = async (user, householdId) => {
-    return await post('/households/' + householdId + '/remove-member', user);
-};
-
-export const deleteHousehold = async (householdId, token) => {
-    //TODO: You can try implement Redis DB. You can get deleted household in the request and store it in Redis DB for if user decides to undo deletion
-
-    return await del('/households/' + householdId + '/delete');
+    return await post(householdEndpoints.removeUserFromHousehold(householdId), user);
 };
 
 export const updateHousehold = async (household, householdId) => {
-    return await patch('/households/' + householdId + '/update', household);
+    return await patch(householdEndpoints.updateHousehold(householdId), household);
+};
+
+export const deleteHousehold = async (householdId) => {
+    //TODO: You can try implement Redis DB. You can get deleted household in the request and store it in Redis DB for if user decides to undo deletion
+    return await del(householdEndpoints.deleteHousehold(householdId));
 };
