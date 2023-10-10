79ba9656990468a6991d64e9c0412f0bf29cb1af
didkoslawow <didkoslawow@gmail.com>
2023-10-10 14:24:37 +0300

    added endpoints objects for requests

---
+++ React JS/React Final Project/rButler/src/api/endpoints.js
@@ -0,0 +1,24 @@
+export const householdEndpoints = {
+    getAllHouseholds: (query) => `/households?search=${query}`,
+    getUserHouseholds: (userId) => `/households/${userId}/user-households`,
+    createHousehold: '/households/create',
+    getUserHouseholdById: '/households/',
+    addUserToHousehold: (householdId) => `/households/${householdId}/add-member`,
+    removeUserFromHousehold: (householdId) => `/households/${householdId}/remove-member`,
+    updateHousehold: (householdId) => `/households/${householdId}/update`,
+    deleteHousehold: (householdId) => `/households/${householdId}/delete`,
+};
+
+export const listsEndpoints = {
+    getListById: '/lists/',
+    createList: '/lists/create',
+    removeList: (listId) => `/lists/${listId}/delete`,
+    addListItem: (listId) => `/lists/${listId}/add-item`,
+    removeListItem: (listId) => `/lists/${listId}/delete-item/`,
+};
+
+export const authEndpoints = {
+    login: '/users/login',
+    register: '/users/register',
+    logout: '/users/logout',
+};
