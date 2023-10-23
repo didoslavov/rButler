f340535807ef4026d80a8a1ada3a24a035fec126
didkoslawow <didkoslawow@gmail.com>
2023-10-23 15:27:11 +0300

    notificationReducer added in redux store

--- React JS/React Final Project/rButler/src/redux/store.js
+++ React JS/React Final Project/rButler/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './reducers';
+import userReducer from './reducers/userReducer';
+import notificationReducer from './reducers/notificationReducer.js';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        notification: notificationReducer,
     },
 });
 
