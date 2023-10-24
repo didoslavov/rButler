9c7633b9fa013c7b622139d5cbb4769cd9f3b60e
didkoslawow <didkoslawow@gmail.com>
2023-10-24 10:43:21 +0300

    added slice for open/close forms in Details

---
+++ React JS/React Final Project/rButler/src/redux/slices/formVisibilitySlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    isEditOpen: false,
+    isCreateOpen: false,
+    isAddMemberOpen: false,
+};
+
+const formVisibilitySlice = createSlice({
+    name: 'formVisibility',
+    initialState,
+    reducers: {
+        setFormVisibility: (state, action) => {
+            const { formType, value } = action.payload;
+            state[formType] = value;
+        },
+    },
+});
+
+export const { setFormVisibility } = formVisibilitySlice.actions;
+export default formVisibilitySlice.reducer;
--- React JS/React Final Project/rButler/src/redux/store.js
+++ React JS/React Final Project/rButler/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './slices/userSlice';
 import notificationSlice from './slices/notificationSlice';
+import formVisibilitySlice from './slices/formVisibilitySlice.js';
 
 const store = configureStore({
     reducer: {
         user: userSlice,
         notification: notificationSlice,
+        formVisibility: formVisibilitySlice,
     },
 });
 
