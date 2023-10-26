8d8e0b0e83fecc8d51a0298445e7bd163ddc9949
didkoslawow <didkoslawow@gmail.com>
2023-10-26 16:43:21 +0300

    using HOC instead of todo and shopping lists

--- React JS/React Final Project/rButler/src/App.jsx
+++ React JS/React Final Project/rButler/src/App.jsx
@@ -1,16 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './components/Home/Home.jsx';
+import Auth from './components/Auth/Auth.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
-import Default from './components/Default/Default.jsx';
+import Home from './components/Home/Home.jsx';
+import AllHouseholds from './components/AllHouseholds/AllHouseholds.jsx';
+import MyHouseholds from './components/MyHouseholds/MyHouseholds.jsx';
 import CreateHouseholdForm from './components/CreateHouseholdForm/CreateHouseholdForm.jsx';
 import Details from './components/Details/Details.jsx';
-import MyHouseholds from './components/MyHouseholds/MyHouseholds.jsx';
-import Auth from './components/Auth/Auth.jsx';
-import ShoppingList from './components/ShoppingList/ShoppingList.jsx';
-import TodoList from './components/TodoList/TodoList.jsx';
-import AllHouseholds from './components/AllHouseholds/AllHouseholds.jsx';
+import List from './components/List/List.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import Default from './components/Default/Default.jsx';
 
 import './App.css';
 
@@ -26,8 +25,8 @@ function App() {
                     <Route path="/households/details/:householdId" element={<Details />} />
                     <Route path="/households/create" element={<CreateHouseholdForm />} />
                     <Route path="/households/:userId" element={<MyHouseholds />} />
-                    <Route path="/lists/shopping/:listId" element={<ShoppingList />} />
-                    <Route path="/lists/todo/:listId" element={<TodoList />} />
+                    <Route path="/lists/shopping/:listId" element={<List type={'shopping'} />} />
+                    <Route path="/lists/todo/:listId" element={<List type={'todo'} />} />
                 </>
                 <Route path="*" element={<Default />}></Route>
             </Routes>
