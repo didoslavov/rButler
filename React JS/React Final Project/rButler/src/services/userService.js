5637d52bf3e99bff0d35935e59ebdb650df5bd10
didkoslawow <didkoslawow@gmail.com>
2023-10-29 14:58:40 +0200

    renaimed user service

---
+++ React JS/React Final Project/rButler/src/services/userService.js
@@ -0,0 +1,30 @@
+import { patch, post } from '../api/requester.js';
+import { userEndpoints } from '../api/endpoints.js';
+
+export async function login(credentials) {
+    const res = await post(userEndpoints.login, credentials);
+
+    if (res.userData) {
+        return res.userData;
+    }
+
+    return res;
+}
+
+export const userRegister = async (credentials) => {
+    const res = await post(userEndpoints.register, credentials);
+
+    if (res.userData) {
+        return res.userData;
+    }
+
+    return res;
+};
+
+export const editUser = async (userData, userId) => {
+    return await patch(userEndpoints.update(userId), userData);
+};
+
+export const logout = async () => {
+    return post(userEndpoints.logout);
+};
