b6c88efa0adf4619ceb2300934082a1c835a0e48
didkoslawow <didkoslawow@gmail.com>
2023-10-29 14:59:55 +0200

    sidebar added to profile page

---
+++ React JS/React Final Project/rButler/src/components/Profile/Sidebar.jsx
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h4 className="sidebar-header">User Profile</h4>
+            <ul className="sidebar-list">
+                <li>Menu Item 2</li>
+                <li>Menu Item 3</li>
+                <li>Edit Profile</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
