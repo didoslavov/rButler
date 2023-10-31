f2b0cc934e82b8fd3aa757f14367f3f9fb2aceb0
didkoslawow <didkoslawow@gmail.com>
2023-10-31 15:13:41 +0200

    added route for reset password

--- React JS/React Final Project/backend/src/routes/usersRoutes.js
+++ React JS/React Final Project/backend/src/routes/usersRoutes.js
@@ -8,6 +8,7 @@ usersRouter.post('/login', loginValidation, usersController.login);
 usersRouter.post('/register', registerValidation, usersController.register);
 usersRouter.post('/logout', usersController.logout);
 usersRouter.patch('/:userId/update', usersController.updateUser);
+usersRouter.patch('/:userId/password-reset', usersController.resetPassword);
 usersRouter.delete('/delete', usersController.deleteUser);
 
 module.exports = usersRouter;
