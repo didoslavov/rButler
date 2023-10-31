d224238a8d7320aa0e5eba385d189912d97df660
didkoslawow <didkoslawow@gmail.com>
2023-10-31 10:52:40 +0200

    fix spacing between avatar file name and X icon

--- React JS/React Final Project/rButler/src/components/Profile/Profile.jsx
+++ React JS/React Final Project/rButler/src/components/Profile/Profile.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Sidebar from './Sidebar.jsx';
 import EditProfile from './EditProfile.jsx';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import ResetPassword from './ResetPassword.jsx';
 
 const Profile = () => {
     return (
         <div className="profile-container">
             <Sidebar />
-            <EditProfile />
+            <Routes>
+                <Route path="edit" element={<EditProfile />} />
+                <Route path="reset-password" element={<ResetPassword />} />
+            </Routes>
         </div>
     );
 };
