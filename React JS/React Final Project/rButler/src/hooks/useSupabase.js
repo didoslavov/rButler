8e931ae0fbb3039ad4cdca53b74555b81b776d36
didkoslawow <didkoslawow@gmail.com>
2023-10-31 10:54:57 +0200

    custom hook for supabase upload implemented

---
+++ React JS/React Final Project/rButler/src/hooks/useSupabase.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+let supabaseInstance = null;
+
+const useSupabase = () => {
+    const [supabase, setSupabase] = useState(null);
+
+    useEffect(() => {
+        if (!supabaseInstance) {
+            const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+            const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
+            supabaseInstance = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+        }
+        setSupabase(supabaseInstance);
+    }, []);
+
+    const uploadAvatar = async (file) => {
+        if (!supabaseInstance) {
+            throw new Error('Supabase client not initialized');
+        }
+
+        const { data, error } = await supabaseInstance.storage.from('avatars').upload(`/${file.name}`, file, {
+            cacheControl: '3600',
+            upsert: false,
+        });
+
+        if (error) {
+            throw new Error(error.message);
+        }
+
+        const avatarURL = `${import.meta.env.VITE_SUPABASE_URL}${import.meta.env.VITE_SUPABASE_BUCKET}${data.path}`;
+        return avatarURL;
+    };
+
+    return { supabase, uploadAvatar };
+};
+
+export default useSupabase;
