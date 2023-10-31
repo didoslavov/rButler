c7d16f4bbeff119660b6562637b6829ff50d9c76
didkoslawow <didkoslawow@gmail.com>
2023-10-31 10:55:24 +0200

    use custom hook for supabase  in edit profile

--- React JS/React Final Project/rButler/src/components/Profile/EditProfile.jsx
+++ React JS/React Final Project/rButler/src/components/Profile/EditProfile.jsx
@@ -10,16 +10,11 @@ import { editUser } from '../../services/userService.js';
 import { setNotification } from '../../redux/slices/notificationSlice.js';
 import { setUser } from '../../redux/slices/userSlice.js';
 
-import { createClient } from '@supabase/supabase-js';
 import { useState } from 'react';
-
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
-const SUPABASE_BUCKET = import.meta.env.VITE_SUPABASE_BUCKET;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+import useSupabase from '../../hooks/useSupabase.js';
 
 const EditProfile = () => {
+    const { uploadAvatar } = useSupabase();
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { register, handleSubmit } = useForm();
@@ -34,22 +29,13 @@ const EditProfile = () => {
         let publicURL = '';
 
         try {
-            if (avatar.length) {
-                const file = avatar[0];
-                const { data, error } = await supabase.storage.from('avatars').upload(`/${file.name}`, file, {
-                    cacheControl: '3600',
-                    upsert: false,
-                });
-
-                if (error) {
-                    throw [error.message];
-                }
-
-                publicURL = SUPABASE_URL + SUPABASE_BUCKET + data.path;
-            }
-
             if (!username || !email) {
                 throw ['All fields are required!'];
+            }
+
+            if (avatar.length) {
+                const file = avatar[0];
+                publicURL = uploadAvatar(file);
             }
 
             const res = await editUser({ username, email, avatar: publicURL }, user.id);
@@ -80,45 +66,33 @@ const EditProfile = () => {
     return (
         <>
             <div className="profile-container">
-                <div className="edit-profile-container">
-                    <h2 className="profile-header">Edit Profile</h2>
-                    {user.avatar && <img src={user.avatar} alt="user avatar" className="avatar" />}
-                    <form className="form-household" onSubmit={handleSubmit(onEditUser)}>
-                        <label>
-                            <span>Name</span>
-                            <input type="text" className="input" {...register('username')} defaultValue={user.username} />
-                        </label>
-                        <label>
-                            <span>Email</span>
-                            <input type="email" className="input" {...register('email')} defaultValue={user.email} />
-                        </label>
-                        <label htmlFor="avatar" className="file-input-label">
-                            <span>Upload Avatar</span>
-                            <input
-                                type="file"
-                                id="avatar"
-                                className="file-input"
-                                {...register('avatar')}
-                                onChange={onChangeFile}
-                            />
-                        </label>
-                        {fileName && (
-                            <div>
-                                <p className="label-file-name">
-                                    <IconButton
-                                        aria-label="close"
-                                        style={{ position: 'absolute', top: 0, right: 0, color: 'var(--dark-blue)' }}
-                                        size="small"
-                                        onClick={handleClearFile}>
-                                        <CloseIcon />
-                                    </IconButton>
-                                    {fileName}
-                                </p>
-                            </div>
-                        )}
-                        <input type="submit" className="submit button" value={'Edit'} />
-                    </form>
-                </div>
+                <h2 className="profile-header">Edit Profile</h2>
+                {user.avatar && <img src={user.avatar} alt="user avatar" className="avatar" />}
+                <form className="form-household" onSubmit={handleSubmit(onEditUser)}>
+                    <label>
+                        <span>Name</span>
+                        <input type="text" className="input" {...register('username')} defaultValue={user.username} />
+                    </label>
+                    <label>
+                        <span>Email</span>
+                        <input type="email" className="input" {...register('email')} defaultValue={user.email} />
+                    </label>
+                    <label htmlFor="avatar" className="file-input-label">
+                        <span>Upload Avatar</span>
+                        <input type="file" id="avatar" className="file-input" {...register('avatar')} onChange={onChangeFile} />
+                    </label>
+                    {fileName && (
+                        <div>
+                            <p className="label-file-name">
+                                <span className="label-text">{fileName}</span>
+                                <IconButton className="label-icon" aria-label="close" size="small" onClick={handleClearFile}>
+                                    <CloseIcon />
+                                </IconButton>
+                            </p>
+                        </div>
+                    )}
+                    <input type="submit" className="submit button" value={'Edit'} />
+                </form>
             </div>
             {notification && <Notification open={open} message={notification} severity={severity} />}
         </>
