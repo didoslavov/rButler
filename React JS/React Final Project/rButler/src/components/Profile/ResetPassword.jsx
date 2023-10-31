084910518735faa5ec6c0cee52ad9a031d8ad8b7
didkoslawow <didkoslawow@gmail.com>
2023-10-31 15:16:01 +0200

    added reset password component

--- React JS/React Final Project/rButler/src/components/Profile/ResetPassword.jsx
+++ React JS/React Final Project/rButler/src/components/Profile/ResetPassword.jsx
@@ -1,26 +1,80 @@
 import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { setNotification } from '../../redux/slices/notificationSlice.js';
+import { useLoading } from '../../hooks/useLoading.js';
+import { resetPassword } from '../../services/userService.js';
+import { setUser } from '../../redux/slices/userSlice.js';
+import { useNavigate } from 'react-router-dom';
+import Notification from '../Notification/Notification.jsx';
 
 const ResetPassword = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [isLoading, handleLoading] = useLoading(false);
+    const { notification, open, severity } = useSelector((state) => state.notification);
+    const { user } = useSelector((state) => state.user);
     const { register, handleSubmit } = useForm();
+
+    const onResetPassword = async ({ oldPass, newPass, rePass }) => {
+        try {
+            await handleLoading(async () => {
+                if (!oldPass || !newPass || !rePass) {
+                    throw ['All fields are required!'];
+                }
+
+                if (newPass !== rePass) {
+                    throw ["Passwords don't match!"];
+                }
+
+                const res = await resetPassword({ oldPass, newPass }, user.id);
+
+                if (res.errors) {
+                    throw res.errors;
+                }
+
+                dispatch(setUser(res.userData));
+                dispatch(
+                    setNotification({
+                        notification: [res.success],
+                        severity: 'success',
+                        open: true,
+                    })
+                );
+                navigate('/profile/edit');
+            });
+        } catch (error) {
+            dispatch(
+                setNotification({
+                    notification: error,
+                    severity: 'error',
+                    open: true,
+                })
+            );
+        }
+    };
+
     return (
-        <div className="profile-container">
-            <h2 className="profile-header">Reset Password</h2>
-            <form className="form-household" onSubmit={handleSubmit()}>
-                <label>
-                    <span>Old Password</span>
-                    <input type="password" className="input" {...register('oldPass')} />
-                </label>
-                <label>
-                    <span>New Password</span>
-                    <input type="password" className="input" {...register('newPass')} />
-                </label>
-                <label>
-                    <span>Repeat Password</span>
-                    <input type="password" {...register('rePass')} />
-                </label>
-                <input type="submit" className="submit button" value={'Edit'} />
-            </form>
-        </div>
+        <>
+            <div className="profile-container">
+                <h2 className="profile-header">Reset Password</h2>
+                <form className="form-household" onSubmit={handleSubmit(onResetPassword)}>
+                    <label>
+                        <span>Old Password</span>
+                        <input type="password" className="input" {...register('oldPass')} />
+                    </label>
+                    <label>
+                        <span>New Password</span>
+                        <input type="password" className="input" {...register('newPass')} />
+                    </label>
+                    <label>
+                        <span>Repeat Password</span>
+                        <input type="password" className="input" {...register('rePass')} />
+                    </label>
+                    <input disabled={isLoading} type="submit" className="submit button" value={'Edit'} />
+                </form>
+            </div>
+            {notification && <Notification open={open} message={notification} severity={severity} />}
+        </>
     );
 };
 
