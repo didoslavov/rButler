e700d6556997908af8428111afd960a12d08bdd4
didkoslawow <didkoslawow@gmail.com>
2023-11-01 08:02:26 +0200

    extract subcontent logic from Auth component

--- React JS/React Final Project/rButler/src/components/Auth/Auth.jsx
+++ React JS/React Final Project/rButler/src/components/Auth/Auth.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import Login from './Login/Login.jsx';
+import SubContent from './SubContent.jsx';
 import Register from './Register/Register.jsx';
 import Notification from '../Notification/Notification.jsx';
 
@@ -18,23 +19,7 @@ const Auth = () => {
             <Login />
 
             <div className="sub-cont">
-                <div className="img">
-                    <div className="img__text m--up">
-                        <h2>Are you a newcomer to these premises?</h2>
-                        <p>Enroll now and unlock a wealth of fresh possibilities!</p>
-                    </div>
-                    <div className="img__text m--in">
-                        <h2>Ah, might you be a part of our distinguished company?</h2>
-                        <p>
-                            Should you be a prior member, a mere sign-in shall suffice. Your presence has been sorely missed, and
-                            we extend a warm welcome back to you.
-                        </p>
-                    </div>
-                    <div className="img__btn" onClick={toggleFormsHandler}>
-                        <span className="m--up">Sign Up</span>
-                        <span className="m--in">Sign In</span>
-                    </div>
-                </div>
+                <SubContent toggleFormsHandler={toggleFormsHandler} />
 
                 <Register />
             </div>
---
+++ React JS/React Final Project/rButler/src/components/Auth/SubContent.jsx
@@ -0,0 +1,23 @@
+const SubContent = ({ toggleFormsHandler }) => {
+    return (
+        <div className="img">
+            <div className="img__text m--up">
+                <h2>Are you a newcomer to these premises?</h2>
+                <p>Enroll now and unlock a wealth of fresh possibilities!</p>
+            </div>
+            <div className="img__text m--in">
+                <h2>Ah, might you be a part of our distinguished company?</h2>
+                <p>
+                    Should you be a prior member, a mere sign-in shall suffice. Your presence has been sorely missed, and we
+                    extend a warm welcome back to you.
+                </p>
+            </div>
+            <div className="img__btn" onClick={toggleFormsHandler}>
+                <span className="m--up">Sign Up</span>
+                <span className="m--in">Sign In</span>
+            </div>
+        </div>
+    );
+};
+
+export default SubContent;
