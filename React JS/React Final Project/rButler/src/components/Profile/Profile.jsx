3405fe6c20f1085baae0eed9b2b4e9fdf03964a6
didkoslawow <didkoslawow@gmail.com>
2023-11-02 16:39:10 +0200

    added route for weather component in the root of Profile

--- React JS/React Final Project/rButler/src/components/Profile/Profile.jsx
+++ React JS/React Final Project/rButler/src/components/Profile/Profile.jsx
@@ -2,12 +2,14 @@ import Sidebar from './Sidebar.jsx';
 import EditProfile from './EditProfile.jsx';
 import { Route, Routes } from 'react-router-dom';
 import ChangePassword from './ChangePassword.jsx';
+import Weather from '../Weather/Weather.jsx';
 
 const Profile = () => {
     return (
         <div className="profile-container">
             <Sidebar />
             <Routes>
+                <Route path="/" element={<Weather />} />
                 <Route path="edit" element={<EditProfile />} />
                 <Route path="reset-password" element={<ChangePassword />} />
             </Routes>
