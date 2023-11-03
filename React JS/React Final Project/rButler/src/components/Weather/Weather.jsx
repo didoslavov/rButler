5ccbbf5b4d87cad511d5081ccbcfd21a7e50c831
didkoslawow <didkoslawow@gmail.com>
2023-11-03 16:33:06 +0200

    extracted weather logic into a custom hook

--- React JS/React Final Project/rButler/src/components/Weather/Weather.jsx
+++ React JS/React Final Project/rButler/src/components/Weather/Weather.jsx
@@ -1,84 +1,12 @@
 import WeatherInfo from './WeatherInfo';
 import DailyForecast from './DailyForecast';
-import { useEffect, useState } from 'react';
-import { searchByCoord, searchLocation } from '../../services/weatherService.js';
 import { useForm } from 'react-hook-form';
-import { useLoading } from '../../hooks/useLoading.js';
 import Spinner from '../LoadingSpinner/Spinner.jsx';
-import { useDispatch } from 'react-redux';
+import { useForecast } from '../../hooks/useForecast.js';
 
 export default function Weather() {
-    const dispatch = useDispatch();
+    const { weather, fiveDays, handleSearch, isLoading } = useForecast();
     const { register, handleSubmit } = useForm();
-    const [weather, setWeather] = useState({ loaded: false });
-    const [fiveDays, setFiveDaysForecast] = useState([]);
-    const [position, setPosition] = useState({});
-    const [isLoading, handleLoading] = useLoading(true);
-
-    const search = async ({ location }) => {
-        try {
-            if (!location) {
-                throw ['Location is required!'];
-            }
-            const { baseForecast, fiveDaysForecast } = await searchLocation(location);
-
-            setWeather({
-                city: baseForecast.name,
-                coordinates: baseForecast.coord,
-                date: new Date(baseForecast.dt * 1000),
-                temperature: Math.round(baseForecast.main.temp),
-                wind: Math.round(baseForecast.wind.speed),
-                humidity: baseForecast.main.humidity,
-                description: baseForecast.weather[0].description,
-                icon: baseForecast.weather[0].icon,
-            });
-            setFiveDaysForecast(fiveDaysForecast.list);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    setPosition({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                    });
-                },
-                (error) => {
-                    console.error(error);
-                }
-            );
-        }
-    }, []);
-
-    useEffect(() => {
-        if (position.latitude && position.longitude) {
-            handleLoading(() => {
-                searchByCoord(position.latitude, position.longitude, dispatch)
-                    .then(({ baseForecast, fiveDaysForecast }) => {
-                        if (baseForecast.cod !== 429 && fiveDaysForecast.cod !== 429) {
-                            setWeather({
-                                city: baseForecast.name,
-                                coordinates: baseForecast.coord,
-                                date: new Date(baseForecast.dt * 1000),
-                                temperature: Math.round(baseForecast.main.temp),
-                                wind: Math.round(baseForecast.wind.speed),
-                                humidity: baseForecast.main.humidity,
-                                description: baseForecast.weather[0].description,
-                                icon: baseForecast.weather[0].icon,
-                            });
-                        }
-                        setFiveDaysForecast(fiveDaysForecast.list);
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    });
-            });
-        }
-    }, [position, dispatch]);
 
     return (
         <>
@@ -87,7 +15,7 @@ export default function Weather() {
             ) : (
                 weather.city && (
                     <div className="Weather">
-                        <form className="search-bar" onSubmit={handleSubmit(search)}>
+                        <form className="search-bar" onSubmit={handleSubmit(handleSearch)}>
                             <div className="search-location">
                                 <input
                                     type="text"
---
+++ React JS/React Final Project/rButler/src/hooks/useForecast.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import { searchByCoord, searchLocation } from '../services/weatherService.js';
+import { useDispatch } from 'react-redux';
+import { useLoading } from './useLoading.js';
+
+export const useForecast = () => {
+    const dispatch = useDispatch();
+    const [weather, setWeather] = useState({ loaded: false });
+    const [fiveDays, setFiveDaysForecast] = useState([]);
+    const [position, setPosition] = useState({});
+    const [isLoading, handleLoading] = useLoading(true);
+
+    const fetchWeatherData = async (latitude, longitude) => {
+        try {
+            const { baseForecast, fiveDaysForecast } = await searchByCoord(latitude, longitude, dispatch);
+            if (baseForecast.cod !== 429 && fiveDaysForecast.cod !== 429) {
+                setWeather({
+                    city: baseForecast.name,
+                    coordinates: baseForecast.coord,
+                    date: new Date(baseForecast.dt * 1000),
+                    temperature: Math.round(baseForecast.main.temp),
+                    wind: Math.round(baseForecast.wind.speed),
+                    humidity: baseForecast.main.humidity,
+                    description: baseForecast.weather[0].description,
+                    icon: baseForecast.weather[0].icon,
+                });
+            }
+            setFiveDaysForecast(fiveDaysForecast.list);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleSearch = async ({ location }) => {
+        try {
+            if (!location) {
+                throw ['Location is required!'];
+            }
+            const { baseForecast, fiveDaysForecast } = await searchLocation(location);
+
+            setWeather({
+                city: baseForecast.name,
+                coordinates: baseForecast.coord,
+                date: new Date(baseForecast.dt * 1000),
+                temperature: Math.round(baseForecast.main.temp),
+                wind: Math.round(baseForecast.wind.speed),
+                humidity: baseForecast.main.humidity,
+                description: baseForecast.weather[0].description,
+                icon: baseForecast.weather[0].icon,
+            });
+            setFiveDaysForecast(fiveDaysForecast.list);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setPosition({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                    });
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+        }
+    }, []);
+
+    useEffect(() => {
+        if (position.latitude && position.longitude) {
+            handleLoading(() => fetchWeatherData(position.latitude, position.longitude));
+        }
+    }, [position, dispatch]);
+
+    return { weather, fiveDays, handleSearch, isLoading };
+};
