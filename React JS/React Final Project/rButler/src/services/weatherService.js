dfb565f96cf14c27e6cc7e5ac66c18a038af971d
didkoslawow <didkoslawow@gmail.com>
2023-11-03 13:19:47 +0200

    implemented caching in weatherService

--- React JS/React Final Project/rButler/src/services/weatherService.js
+++ React JS/React Final Project/rButler/src/services/weatherService.js
@@ -1,5 +1,10 @@
-const apiKey = '65c8bce3c3835f283c727ae2bbd5cf75';
-const baseUrl = 'https://api.openweathermap.org/data/2.5';
+import { updateWeatherData } from '../redux/slices/weatherSlice.js';
+
+const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const baseUrl = import.meta.env.VITE_OPENWEATHER_BASE_URL;
+
+const refreshCacheTimeStamp = 900000; // 15min. refresh interval
+let weatherDataCache = {};
 
 export async function searchLocation(city) {
     const res = await fetch(`${baseUrl}/weather?q=${city}&appid=${apiKey}&units=metric`);
@@ -13,14 +18,56 @@ export async function searchLocation(city) {
     return { baseForecast, fiveDaysForecast };
 }
 
-export async function searchByCoord(lat, lon) {
-    const res = await fetch(`${baseUrl}/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
-    const baseForecast = await res.json();
+export const searchByCoord = async (lat, lon, dispatch) => {
+    const cachedWeatherData = weatherDataCache[lat]?.[lon];
+    const currentTime = new Date().getTime();
 
-    const fiveDaysForecast = await getFiveDaysForecast(lat, lon);
+    if (cachedWeatherData && currentTime - cachedWeatherData.timestamp < refreshCacheTimeStamp) {
+        dispatch(
+            updateWeatherData({
+                lat,
+                lon,
+                weatherData: cachedWeatherData.data,
+                timestamp: cachedWeatherData.timestamp,
+                fiveDaysForecast: cachedWeatherData.fiveDaysForecast,
+            })
+        );
 
-    return { baseForecast, fiveDaysForecast };
-}
+        return {
+            baseForecast: cachedWeatherData.data,
+            fiveDaysForecast: { list: cachedWeatherData.fiveDaysForecast },
+        };
+    } else {
+        try {
+            const res = await fetch(`${baseUrl}/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+            const baseForecast = await res.json();
+
+            const fiveDaysForecast = await getFiveDaysForecast(lat, lon);
+
+            weatherDataCache[lat] = {
+                [lon]: {
+                    data: baseForecast,
+                    timestamp: currentTime,
+                    fiveDaysForecast: fiveDaysForecast.list,
+                },
+            };
+
+            dispatch(
+                updateWeatherData({
+                    lat,
+                    lon,
+                    weatherData: baseForecast,
+                    timestamp: currentTime,
+                    fiveDaysForecast: fiveDaysForecast.list,
+                })
+            );
+
+            return { baseForecast, fiveDaysForecast };
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
 
 async function getFiveDaysForecast(lat, lon) {
     const res = await fetch(`${baseUrl}/forecast?lat=${lat}&lon=${lon}&cnt=5&appid=${apiKey}&units=metric`);
