f1be3faa22dfefedd8f939ecdacbe1261ccf8640
didoslavov <didkoslawow@gmail.com>
2023-11-15 20:02:13 +0200

    added new domain to allowed headers in cors middleware

--- server/src/middlewares/cors.js
+++ server/src/middlewares/cors.js
@@ -1,8 +1,15 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    const allowedOrigins = ['http://localhost:5173', 'https://rbutler-api.onrender.com'];
+
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Auth');
     res.setHeader('access-control-expose-headers', 'Set-Cookie');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     next();
 };
