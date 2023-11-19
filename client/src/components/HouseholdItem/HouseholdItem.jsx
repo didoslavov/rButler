827b2bbdd3f8ab3ce7492694e7ec8fbb43d02347
didoslavov <didkoslawow@gmail.com>
2023-11-19 12:33:33 +0200

    separate css for HouseholdItem and HouseholdList

--- client/src/components/HouseholdItem/HouseholdItem.jsx
+++ client/src/components/HouseholdItem/HouseholdItem.jsx
@@ -7,7 +7,7 @@ const HouseholdItem = ({ household }) => {
             <div className="chip-container">
                 <Chip sx={chipStyles} label={'Owner: ' + household.master.username} />
             </div>
-            <h4 className="household-header border-bottom">{household.name}</h4>
+            <h4 className="border-bottom">{household.name}</h4>
             <p>{household.presentation}</p>
         </li>
     );
