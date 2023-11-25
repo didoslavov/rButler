31b7e3ffa663bf8839fad24110b0b5f4debb317d
didoslavov <didkoslawow@gmail.com>
2023-11-25 18:42:05 +0200

    added vitest to the project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,5 +8,11 @@ export default defineConfig({
     plugins: [react()],
     optimizeDeps: {
         include: ['@emotion/styled'],
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        css: true,
+        setupFiles: './setup.js',
     },
 });
