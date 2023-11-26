cea0802e94c4f4f846f8f0ad08a66805fcc89c43
didoslavov <didkoslawow@gmail.com>
2023-11-26 12:26:40 +0200

    added tests for Atuh component

---
+++ client/src/components/Auth/Auth.test.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Auth from './Auth';
+import store from '../../redux/store';
+import { vi } from 'vitest';
+import { createMemoryHistory } from 'history';
+import * as userService from '../../services/userService.js';
+
+const userServiceWrapper = {
+    ...userService,
+    login: () => Promise.resolve({ username: 'testuser', avatar: 'testavatar' }),
+    userRegister: () => Promise.resolve({ username: 'testuser', avatar: 'testavatar' }),
+};
+
+describe('Auth Component', () => {
+    it('renders the Auth component with Login form by default', () => {
+        render(
+            <Provider store={store}>
+                <BrowserRouter history={createMemoryHistory()}>
+                    <Auth />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        waitFor(() => {
+            expect(screen.getByText('Your return is a delight!')).toBeInTheDocument();
+            expect(screen.getByLabelText('Username')).toBeInTheDocument();
+            expect(screen.getByLabelText('Password')).toBeInTheDocument();
+            expect(screen.getByText('Forgot password?')).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: 'Sign In' })).toBeInTheDocument();
+
+            expect(screen.queryByText('Step inside, dear newcomer!')).toBeNull();
+        });
+    });
+
+    it('renders the Register form after clicking on the "Sign Up" button', () => {
+        render(
+            <Provider store={store}>
+                <BrowserRouter history={createMemoryHistory()}>
+                    <Auth />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        waitFor(() => {
+            fireEvent.click(screen.getByText('Sign Up'));
+
+            expect(screen.getByText('Step inside, dear newcomer!')).toBeInTheDocument();
+            expect(screen.getByLabelText('Username')).toBeInTheDocument();
+            expect(screen.getByLabelText('Email')).toBeInTheDocument();
+            expect(screen.getByLabelText('Password')).toBeInTheDocument();
+            expect(screen.getByLabelText('Repeat Password')).toBeInTheDocument();
+            expect(screen.getByLabelText('Choose Avatar')).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: 'Sign Up' })).toBeInTheDocument();
+
+            expect(screen.queryByText('Your return is a delight!')).toBeNull();
+        });
+    });
+
+    it('renders the Login form after clicking on the "Sign In" button', async () => {
+        render(
+            <Provider store={store}>
+                <BrowserRouter history={createMemoryHistory()}>
+                    <Auth />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        waitFor(() => {
+            fireEvent.click(screen.getByText('Sign Up'));
+
+            fireEvent.click(screen.getByText('Sign In'));
+
+            expect(screen.getByText('Your return is a delight!')).toBeInTheDocument();
+            expect(screen.getByLabelText('Username')).toBeInTheDocument();
+            expect(screen.getByLabelText('Password')).toBeInTheDocument();
+            expect(screen.getByText('Forgot password?')).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: 'Sign In' })).toBeInTheDocument();
+
+            expect(screen.queryByText('Step inside, dear newcomer!')).toBeNull();
+        });
+    });
+
+    it('handles form submission with valid data for Login', async () => {
+        vi.spyOn(userService, 'login').mockImplementation(userServiceWrapper.login);
+
+        render(
+            <Provider store={store}>
+                <BrowserRouter history={createMemoryHistory()}>
+                    <Auth />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        waitFor(() => {
+            fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
+            fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'testpassword' } });
+
+            fireEvent.click(screen.getByRole('button', { name: 'Sign In' }));
+
+            expect(userService.login).toHaveBeenCalledWith({
+                username: 'testuser',
+                password: 'testpassword',
+            });
+        });
+    });
+
+    it('handles form submission with valid data for Register', async () => {
+        vi.spyOn(userService, 'userRegister').mockImplementation(userServiceWrapper.userRegister);
+
+        render(
+            <Provider store={store}>
+                <BrowserRouter history={createMemoryHistory()}>
+                    <Auth />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        waitFor(() => {
+            fireEvent.click(screen.getByText('Sign Up'));
+
+            fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'testuser' } });
+            fireEvent.change(screen.getByLabelText('Email'), { target: { value: 'test@email.com' } });
+            fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'testpassword' } });
+            fireEvent.change(screen.getByLabelText('Repeat Password'), { target: { value: 'testpassword' } });
+
+            fireEvent.click(screen.getByRole('button', { name: 'Sign Up' }));
+
+            expect(userService.userRegister).toHaveBeenCalledWith({
+                username: 'testuser',
+                email: 'test@email.com',
+                password: 'testpassword',
+                repass: 'testpassword',
+                avatar: '',
+            });
+        });
+    });
+});
